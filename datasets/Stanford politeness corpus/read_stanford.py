4d5edb023bf57639153a625694a9629e1379a175
Carla Groenland <carla.groenland@gmail.com>
2014-11-04 12:40:04 +0100

    sort on mean score and print corresponding sentences

--- datasets/Stanford politeness corpus/read_stanford.py
+++ datasets/Stanford politeness corpus/read_stanford.py
@@ -1,20 +1,26 @@
 import csv
 import sys
+import numpy as np
 
 maxInt = int(sys.maxsize/(10**10))
 csv.field_size_limit(maxInt)
 
-with open('stack-exchange.annotated.csv') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=',')
-	rowCounter = 0
-	for row in spamreader:
-		rowCounter+1
-		counter = 0
-		for col in row:
-			if counter == 0:
-				print col
-				break
-			counter = counter+1
-	print rowCounter
+def read_column(colNr):
+	with open('stack-exchange.annotated.csv') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter=',')
+		row = next(spamreader)
+		column = []
+		for row in spamreader:
+			counter = 0
+			for col in row:
+				if counter == colNr:
+					column.append(col)
+					break
+				counter = counter+1
+		return column
 
-
+col1 = np.array(read_column(2)) # sentences
+col2 = np.array(read_column(13)) # mean score
+indices = np.array(np.argsort(col2))
+print col2[indices]
+print col1[indices]
