70bafc3685170827b3de0271645e991f2a59b192
Carla Groenland <carla.groenland@gmail.com>
2014-11-05 17:12:42 +0100

    Divided data in train and test data (with equal distributions for wikipedia and stackoverflow). Made "Post" class for storing the posts.

---
+++ code/post.py
@@ -0,0 +1,34 @@
+import csv
+import sys
+import numpy as np
+
+class Post(object):
+	def __init__(self, content, score, community):
+		self.content = content
+		self.score = score
+		self.community = community
+
+
+maxInt = int(sys.maxsize/(10**10))
+csv.field_size_limit(maxInt)
+
+def read_column(colNr, fileName):
+	with open(fileName) as csvfile:
+		readerObject = csv.reader(csvfile, delimiter=',')
+		row = next(readerObject)
+		column = []
+		for row in readerObject:
+			counter = 0
+			for col in row:
+				if counter == colNr:
+					column.append(col)
+					break
+				counter = counter+1
+		return column
+
+contents = read_column(0,'train.csv')
+scores = read_column(1,'train.csv')
+communities = read_column(2,'train.csv')
+print contents[0:2]
+print scores[0:2]
+print communities[0:2]
