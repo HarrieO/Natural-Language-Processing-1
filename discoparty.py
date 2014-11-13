da84e81e8167426bfcee9a212f24148e374fb079
Ties van Rozendaal <tiesvanrozendaal@wcw-stud-145-109-5-236.wireless.uva.nl>
2014-11-13 12:00:14 +0100

    Discodop Test

---
+++ discoparty.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# coding=utf-8
+
+import discodop
+
+def load_all(directory):
+    for loader, name, ispkg in pkgutil.walk_packages([directory]):
+        module = loader.find_module(name).load_module(name)
+        exec('%s = module' % name)
+
+if __name__ == '__main__':
+	print 'Discodop test!!'
+
+	load_all(discodop)
+
+	dir(discodop)
