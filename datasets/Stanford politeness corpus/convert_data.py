1a4f55a16f87397d3f9cbeae15b7fd680cb1efc6
HarrieO <harrieoosterhuis@gmail.com>
2014-11-13 12:02:14 +0100

    indented equal signs

--- datasets/Stanford politeness corpus/convert_data.py
+++ datasets/Stanford politeness corpus/convert_data.py
@@ -32,39 +32,39 @@ def make_list(col1,col2,col3, indices):
 	return combinedList
 
 # Read and visualise all data
-comminutySE = np.array(read_column(0,'stack-exchange.annotated.csv')) # community
-sentencesSE = np.array(read_column(2,'stack-exchange.annotated.csv')) # sentences
+comminutySE  = np.array(read_column(0,'stack-exchange.annotated.csv')) # community
+sentencesSE  = np.array(read_column(2,'stack-exchange.annotated.csv')) # sentences
 scoresSElist = read_column(13,'stack-exchange.annotated.csv') # mean score
-scoresSE = np.zeros(len(scoresSElist))
+scoresSE     = np.zeros(len(scoresSElist))
 for i in range(len(scoresSElist)):
-	scoresSE[i]=scoresSElist[i]
+	scoresSE[i] = scoresSElist[i]
 indicesSE = np.array(np.argsort(scoresSE))
 print sentencesSE[indicesSE]
 print scoresSE[indicesSE]
 print comminutySE[indicesSE]
-comminutyWIK = np.array(read_column(0,'wikipedia.annotated.csv')) # sentences
-sentencesWIK = np.array(read_column(2,'wikipedia.annotated.csv')) # sentences
+comminutyWIK  = np.array(read_column(0,'wikipedia.annotated.csv')) # sentences
+sentencesWIK  = np.array(read_column(2,'wikipedia.annotated.csv')) # sentences
 scoresWIKlist = read_column(13,'wikipedia.annotated.csv') # mean score
-scoresWIK = np.zeros(len(scoresWIKlist))
+scoresWIK     = np.zeros(len(scoresWIKlist))
 for i in range(len(scoresWIKlist)):
-	scoresWIK[i]=scoresWIKlist[i]
+	scoresWIK[i] = scoresWIKlist[i]
 indices = np.array(np.argsort(scoresWIK))
 print scoresWIK[indices]
 print sentencesWIK[indices]
 
 # Distribute into train and test data (indices)
 randomIndices = np.random.permutation(indicesSE)
-switchSE = int(np.floor(0.8*indicesSE.shape[0]))
-trainSE = randomIndices[:switchSE]
-testSE = randomIndices[switchSE:]
-trainData1 = make_list(sentencesSE,scoresSE,comminutySE, trainSE)
-testData1 = make_list(sentencesSE,scoresSE,comminutySE, testSE)
+switchSE      = int(np.floor(0.8*indicesSE.shape[0]))
+trainSE       = randomIndices[:switchSE]
+testSE 		  = randomIndices[switchSE:]
+trainData1    = make_list(sentencesSE,scoresSE,comminutySE, trainSE)
+testData1     = make_list(sentencesSE,scoresSE,comminutySE, testSE)
 randomIndices = np.random.permutation(indices)
-switchWIK = int(np.floor(0.8*indices.shape[0]))
-trainWIK = randomIndices[:switchWIK]
-testWIK = randomIndices[switchWIK:]
-trainData2 = make_list(sentencesWIK,scoresWIK,comminutyWIK, trainWIK)
-testData2 = make_list(sentencesWIK,scoresWIK,comminutyWIK, testWIK)
+switchWIK     = int(np.floor(0.8*indices.shape[0]))
+trainWIK      = randomIndices[:switchWIK]
+testWIK       = randomIndices[switchWIK:]
+trainData2    = make_list(sentencesWIK,scoresWIK,comminutyWIK, trainWIK)
+testData2     = make_list(sentencesWIK,scoresWIK,comminutyWIK, testWIK)
 
 # write to csv file
 write_csv(trainData1+trainData2,'train.csv')
