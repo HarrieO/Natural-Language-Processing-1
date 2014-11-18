b25f932daed5904bcffa03ce879a9af4d7e85a7c
Ties van Rozendaal <tiesvanrozendaal@wcw-stud-145-109-5-236.wireless.uva.nl>
2014-11-18 14:26:01 +0100

    dop_features gaat de goede kant op.
    run:
    python dop_features.py 3
    
    om het voorbeeld van datapoint 3 te zien

--- code/disco/dop_features.py
+++ code/disco/dop_features.py
@@ -1,14 +1,53 @@
 #!/usr/bin/env python2
 # coding=utf-8
-import glob, sys
+import glob, sys, re, argparse
+import numpy as np
 from discodop import treebank, treetransforms, fragments
 from sklearn import linear_model, preprocessing, feature_extraction, cross_validation
 
-print 'Hello world'
-
+# In order to import the main project code
 sys.path.append('..')
-
+# import the data reader
 import post 
+
+#The tokenizer which splits sentences
+def split_sentences(txt): return (re.findall(r'(?ms)\s*(.*?(?:\.|\?|!))', txt))  # split sentences
+
+# Read the comments in.
+contents    = post.read_column(0,'../train.csv')
+
+#preallocate counter for the number of sentences per datapoint
+n_sents = np.ones(len(contents),dtype=int)
+
+#for each datapoint split the datapoint, add to the file and store the number of sentences
+for n,datapoint in enumerate(contents):
+	n_sents[n] = len(split_sentences(datapoint))
+
+#Display statistics
+print 'Datapoints: {0} \n Sentences: {1}'.format(len(n_sents),sum(n_sents))
+
+
+
+
+# DISPLAY:
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                   help='The datapoint to display')
+
+    args = parser.parse_args()
+
+    n =  args.integers[0]
+
+    zin = contents[n]
+    print zin
+    print split_sentences(zin)
+    print 'Sentences detected: {0}'.format(len(split_sentences(zin)))
+
+# END OF DISPLAY
+
+
+
 #Per datapunt:
 #> Vind elke zin ? . NOT .” ?”
 #> Zet elke zin binnen <s> </s> TAGS
