c38616e66100d0888604b5c143a58676c7f5ec99
HarrieO <harrieoosterhuis@gmail.com>
2014-11-24 23:54:10 +0100

    dop data converted to feature space

--- code/disco/treedataToFeatures.py
+++ code/disco/treedataToFeatures.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 # coding=utf-8
-import os, glob, sys, re, argparse,shutil
+import os, glob, sys, re, argparse,shutil, csv
 import numpy as np
 from discodop import treebank, treetransforms, fragments
 from sklearn import linear_model, preprocessing, feature_extraction, cross_validation
@@ -28,16 +28,36 @@ print "Starting fragment extraction"
 result = fragments.getfragments(trees, sents, numproc=1, disc=False, cover=True)
 print "Extracted fragments"
 featureMatrix = [{} for _ in range(len(treeposts))]
+treeToIndices = []
+treeIndex = 0
 for tree, sentDict in result.items():
+    treeToIndices.append(tree)
     #print '%3d\t%s' % (sum(b.values()), a)
     for key, count in sentDict.items():
         if tree in treeposts[indices[key]].fragments:
-            treeposts[indices[key]].fragments[tree] += count
-            featureMatrix[indices[key]][tree] += count
+            treeposts[indices[key]].fragments[treeIndex]    += count
+            featureMatrix[indices[key]][tree]               += count
         else:
-            treeposts[indices[key]].fragments[tree] = count
-            featureMatrix[indices[key]][tree] = count
-     
+            treeposts[indices[key]].fragments[treeIndex]    = count
+            featureMatrix[indices[key]][tree]               = count
+    treeIndex += 1
+   
+with open("featureData.csv", 'wb') as csvfile:
+    writerObject = csv.writer(csvfile, delimiter=',')
+    for post in treeposts:
+        writerObject.writerow([post.id, post.content, post.score, post.community, post.fragments])  
+
+
+file = open("indicesToTrees.txt", "w")
+for i in range(treeIndex):
+    print str(i),treeToIndices[i]
+    file.write(str(i) + ' ')
+    file.write(treeToIndices[i].encode("utf-8"))
+    file.write('\n')
+
+#[file.write((str(i) + ' ' + str(treeToIndices[i]) + u'\n').encode('utf8')) for i in range(treeIndex)]
+file.close()
+
 print "Added fragments to posts"
 
 # Convert list of dicts to a sparse matrix
@@ -45,7 +65,14 @@ vectorizer = feature_extraction.DictVectorizer(sparse=True)
 X = vectorizer.fit_transform(featureMatrix)
     
 # Trivial machine learning objective: detect long sentences
-target = ['polite' if post.score > 0 else 'impolite' for post in treeposts]
+target = []
+for post in treeposts:
+    if post.score > 0.5:
+        target.append('polite')
+    elif post.score > -0.5:
+        target.append('neutral')
+    else:
+        target.append('impolite')
 y = preprocessing.LabelEncoder().fit_transform(target)
 
 # Use an SVM-like classifier and 10-fold crossvalidation for evaluation
