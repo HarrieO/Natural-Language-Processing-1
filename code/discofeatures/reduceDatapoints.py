db292f67e71dc9e214cb952f58496dcaecaff259
Carla Groenland <carla.groenland@gmail.com>
2014-11-28 11:30:30 +0100

    computed entropy for features, computation is now pretty fast!

--- code/discofeatures/reduceDatapoints.py
+++ code/discofeatures/reduceDatapoints.py
@@ -1,5 +1,11 @@
 from computeEntropy import *
 from datapoint import *
+import collections as col
+
+def entropy(countPerClass):
+	probs = countPerClass/float(np.sum(countPerClass))
+	probs = probs[probs>0.0]
+	return -np.sum(probs*np.log(probs))
 
 # Returns to which class the score belongs
 def getClass(score,classCutOff,classes):
@@ -9,32 +15,29 @@ def getClass(score,classCutOff,classes):
 			i = i + 1
 	return classes[i]
 
-def reduceDatapoints(fileName='featureData.csv', classes=['negative','neutral','positive'], classCutOff=[-0.5,0.5],fractionKept=0.01):
+def reduceDatapoints(fileName='featureData.csv', classes=[0,1,2], classCutOff=[-0.5,0.5],fractionKept=0.01, start=0, end=270000):
 	print "Reading data"
-	posts = read_posts(fileName)
+	posts = read_data(fileName)
 	print "Finished reading data, started accumulating counts"
-	classCount = dict()
-	for c in classes:
-		classCount[c] = 0
-	wordClassCount = dict()
+	wordCountClasses = np.zeros([len(classes),end])
+	classCount = np.zeros(len(classes))
 	for post in posts:
-		words = post.fragments.keys()
 		currClass = getClass(float(post.score), classCutOff, classes)
 		classCount[currClass]+=1
-		for word in words:
-			if not(word in wordClassCount.keys()):
-				wordClassCount[word]=dict()
-				for entry in classes:
-					wordClassCount[word][entry] = 0
-			wordClassCount[word][currClass]+=1
-	print "Computing entropy per feature"
-	gainPerFeature = word_entropy(classCount, wordClassCount, True)
-
-	ordered = sorted(gainPerFeature, key=gainPerFeature.get, reverse =True) # list of features ordered by gain
-	keepFeatures = ordered[:int(fractionKept*len(ordered))]
-	file = open("sortedIndices.txt", "w")
+		wordCountClasses[currClass][np.array(post.fragments.keys())]+=1
+	print "Computing entropy per class"
+	initialEntropy = entropy(classCount)
+	print "Result: ", initialEntropy
+	print "Computing information gain per feature for features ", start, " to ",end
+	countWord =np.sum(wordCountClasses,0) 
+ 	probWord = countWord/ float(np.sum(classCount)) # probability a feature occurs in sentence
+	dictWord = dict()
+	for i in range((countWord[countWord!=0]).shape[0]):
+	 	wordEntropy = entropy(wordCountClasses[:,i])*probWord[i] +(1.0-probWord[i]) *entropy(classCount-wordCountClasses[:,i])
+		dictWord[i] = wordEntropy-initialEntropy 
+	ordered = sorted(dictWord, key=dictWord.get)
+	file = open("informationGain.txt", "w")
 	for feature in ordered:
-		file.write(str(feature)+','+str(gainPerFeature[feature])+ '\n')
+	 	file.write(str(feature)+','+str(dictWord[feature])+ '\n')
 	file.close()
-	return keepFeatures
-keepFeatures = reduceDatapoints()
+reduceDatapoints()
