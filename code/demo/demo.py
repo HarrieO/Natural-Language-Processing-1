10a1145e485b8daefc793e45cd6da3e247e29d75
Joost van Doorn <joost.van.doorn@gmail.com>
2014-12-01 19:06:01 +0100

    Demo code

--- code/demo/demo.py
+++ code/demo/demo.py
@@ -1,18 +1,27 @@
 import tornado.ioloop
 import tornado.web
 import os.path
+import sys
 from tornado.options import define, options, parse_command_line
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
+class PolitenessHandler(tornado.web.RequestHandler):
+    def post(self):
+    	sentence = self.get_argument("sentence", None)
+        response = { 'sentences': [ {'sentence': " ".join(["<span class=\"neutral\">"+word+"</span>" for word in sentence.split(" ") if word != "" ]), 'sentenceClass': 'polite' } ]}
+        self.write(response)
 
 application = tornado.web.Application([
-    (r"/", MainHandler)],
+    (r"/politeness/", MainHandler),
+    (r"/politeness/classify/", PolitenessHandler)],
     template_path=os.path.join(os.path.dirname(__file__), "templates"),
     static_path=os.path.join(os.path.dirname(__file__), "static"),
     xsrf_cookies=True,
-    debug=True
+    debug=True,
+    static_url_prefix="/politeness/static/"
+
 )
 
 if __name__ == "__main__":
--- code/demo/static/demo.js
+++ code/demo/static/demo.js
@@ -1,19 +1,61 @@
 $(function () { 
-
-$("form").submit(function( event ) {
-    showSentence('<span class="neutral">You</span> <span class="neutral">are</span> <span class="neutral">not</span> <span class="polite">very</span> <span class="neutral">helpful</span><span class="neutral">,</span> <span class="neutral">what</span> <span class="neutral">the</span> <span class="impolite">hell</span> <span class="neutral">are</span> <span class="neutral">you</span> <span class="neutral">doing</span><span class="neutral">?</span>');
-
-  event.preventDefault();
+    $("form").submit(function( event ) {
+        event.preventDefault();
+        $.ajax({
+          url: "classify/",
+          type: 'post',
+          data: $("form").serialize(),
+          dataType: 'json',
+          success: (function(data) {
+            var sentences = data.sentences;
+            for (i = 0; i < sentences.length; i++) { 
+                console.log(sentences[i].sentenceClass);
+                showSentence(""+sentences[i].sentence, sentences[i].sentenceClass);
+            }
+            
+            }),
+          complete:  (function(data) {
+            console.log("Fail");
+            }),
+        });
+          
+    });
 });
-    // 
-   });
+function showSentence(sentence, sentenceClass) {
+    if(sentence == "") {
+        $("#results .sentence-result").html('<div class="no-input">Do you want to know how I work? Fill in a sentence first!</div>');
+        $("#results .sentence-result").lettering('words');
+        updateClass("neural");
+    }
+    else {
+        $("#results .sentence-result").html(sentence);
+        console.log(sentenceClass);
+        updateClass(sentenceClass);
+    }
+    $("#results .sentence-result span").lettering();
+    var tl = new TimelineMax();
+    TweenLite.set("#results", {perspective:600, autoAlpha:1});
+    tl.staggerFrom($("#results"), 0.2, {scaleY:0, overflow:"hidden",ease:Circ.easein}, "+=0");
+    tl.staggerFrom($("#results .sentence-result span"), 0.2, {opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"50% 50% -50",  ease:Back.easeOut}, 0.005, "+=0");
 
-function showSentence(sentence) {
-    $("#results .sentence-result").html(sentence);
-    $("#results .sentence-result span").lettering()
-var tl = new TimelineMax();
-TweenLite.set("#results", {perspective:600, autoAlpha:1});
-tl.staggerFrom($("#results"), 0.2, {scaleY:0, overflow:"hidden",ease:Circ.easein}, "+=0");
-tl.staggerFrom($("#results .sentence-result span"), 0.2, {opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"50% 50% -50",  ease:Back.easeOut}, 0.005, "+=0");
-
+}
+function updateClass(newClass) {
+    if(newClass == "polite") {
+        $("#results .class-label").addClass("label-success");
+        $("#results .class-label").removeClass("label-default");
+        $("#results .class-label").removeClass("label-danger");
+        $("#results .class-label").html("Polite");
+    }
+    else if(newClass == "impolite") {
+        $("#results .class-label").removeClass("label-success");
+        $("#results .class-label").removeClass("label-default");
+        $("#results .class-label").addClass("label-danger");
+        $("#results .class-label").html("Impolite");
+    }
+    else {
+        $("#results .class-label").removeClass("label-success");
+        $("#results .class-label").addClass("label-default");
+        $("#results .class-label").removeClass("label-danger");
+        $("#results .class-label").html("Neural");
+    }
 }
