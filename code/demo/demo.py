a6b1cc3d9f69b2e9c28ed978a9274d564d119945
Joost van Doorn <joost.van.doorn@gmail.com>
2014-12-03 22:57:45 +0100

    Fixed bllip python library

--- code/demo/demo.py
+++ code/demo/demo.py
@@ -2,25 +2,38 @@ import tornado.ioloop
 import tornado.web
 import os.path
 import sys
+import time
+from threading import Thread
 from tornado.options import define, options, parse_command_line
 sys.path.append(os.path.join(os.path.dirname(__file__), '../disco'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 from settings import *
+
+
 if USE_BLLIP == True:
-	from get_trees import *
+	if BLLIP_TYPE == 'Python':
+		from bllipparser import RerankingParser, tokenize
+	else:
+		from get_trees import *
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 class PolitenessHandler(tornado.web.RequestHandler):
     def post(self):
-    	global USE_BLLIP
+    	global USE_BLLIP, BLLIP_TYPE, rrp
     	sentence = self.get_argument("sentence", None)
     	if USE_BLLIP == True:
-    		sentence = get_trees(sentence)
+		if BLLIP_TYPE == 'Python':
+			sentence = rrp.simple_parse(str(sentence))
+		else:
+    			sentence = get_trees(sentence)
+    			sentence = sentence[0]
+		# Test trees (S1 (S (VP (VB Thank) (NP (PRP you)) (PP (IN for) (NP (DT the) (FW response.) (SQ (MD Would) (NP (PRP you)) (VP (AUX be) (ADJP (JJ willing) (S (VP (TO to) (VP (VB add) (NP (DT a) (JJ few) (JJR more) (NNS details)) (S (VP (TO to) (VP (VB explain) (ADVP (RBR further)))))))))))))) (. ?)))
+		# (S1 (SBARQ (WHNP (WDT That)) (SQ (AUX 's) (NP (NP (DT the) (JJ only) (NN answer)) (SBAR (S (NP (PRP you)) (VP (AUX have)))) (. ?)) (ADVP (RB seriously)) (. ?) (SQ (MD can) (RB n't) (NP (PRP you)) (VP (AUX do) (ADVP (RB better))))) (. ?)))
     	else:
-    		sentence = [sentence]
-    	sentence = sentence[0]
+    		sentence = sentence
         response = { 'sentences': [ {'sentence': " ".join(["<span class=\"neutral\">"+word+"</span>" for word in sentence.split(" ") if word != "" ]), 'sentenceClass': 'polite' } ]}
         self.write(response)
 
@@ -35,6 +48,19 @@ application = tornado.web.Application([
 
 )
 
+class bllip_loader(Thread):
+    def run(self):
+	global rrp
+	print "Reloading"
+	rrp = RerankingParser()
+	rrp.load_parser_model('../../lib/bllip/DATA/EN')
+	rrp.load_reranker_model('../../lib/bllip/models/ec50spfinal/features.gz', '../../lib/bllip/models/ec50spfinal/cvlm-l1c10P1-weights.gz')
+	print "Done loading model"
+
+
 if __name__ == "__main__":
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+	application.listen(8888)
+	if USE_BLLIP == True:
+		if BLLIP_TYPE == 'Python':
+			bllip_loader().start()
+	tornado.ioloop.IOLoop.instance().start()
