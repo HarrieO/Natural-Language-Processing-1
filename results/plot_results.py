4d550e5b8db827827fffefef3e740a468f9c5545
Ties van Rozendaal <tiesvanrozendaal@Tiess-MacBook-Pro.local>
2014-12-04 15:44:18 +0100

    Fixed plot function to deal with MSE for regression

--- results/plot_results.py
+++ results/plot_results.py
@@ -25,7 +25,7 @@ def get_classifier_table(classifier_name,sort_ind=5):
 	Default sort = column 5 (features)
 	"""
 
-	classifier_settings = ''
+	classifier_settings = '.*' #not implemented yet
 
 	#get all lines with current classifier and current settings
 	regexp = settings_to_string(classifier_name,".*",".*",".*",".*",".*",classifier_settings)
@@ -33,6 +33,7 @@ def get_classifier_table(classifier_name,sort_ind=5):
 		lines = [line for line in fd if re.search(regexp, line) != None]
 
 	table = [];
+
 	for line in lines:
 		v = line.split(',')
 		#v[0] #classifier
