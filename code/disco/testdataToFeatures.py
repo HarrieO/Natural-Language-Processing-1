6554e546635a552f8740a644664ccc46078d9b69
HarrieO <harrieoosterhuis@gmail.com>
2014-12-15 11:06:57 +0100

    full testset

--- code/disco/testdataToFeatures.py
+++ code/disco/testdataToFeatures.py
@@ -26,7 +26,7 @@ vectorizer = feature_extraction.DictVectorizer(sparse=True)
 
 treeStrings = [ line[:-1] for line in io.open('../../datasets/preprocessed/test_trees.txt', encoding='utf-8')]
 print "Total of", len(treeStrings), "trees in test set."
-# treeStrings.extend([ line[:-1] for line in io.open('../../datasets/preprocessed/trees.txt', encoding='utf-8')])
+treeStrings.extend([ line[:-1] for line in io.open('../../datasets/preprocessed/trees.txt', encoding='utf-8')])
 
 text = BracketStringReader(treeStrings)
 print "Made treebank"
