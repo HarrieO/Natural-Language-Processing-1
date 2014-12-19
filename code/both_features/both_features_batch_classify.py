07a691f7cc0f6cc0a73da00bde61195158e7d3a7
Ties van Rozendaal <git@tivaro.nl>
2014-12-19 17:57:04 +0100

    Modiefied the scripts to deal with manual specification of word and dop proportions. Files are now also keeping track of this

--- code/both_features/both_features_batch_classify.py
+++ code/both_features/both_features_batch_classify.py
@@ -21,13 +21,15 @@ def both_features_batch_run(test_settings,method=2):
 
 
 	#initialize
-	last_features = [];
+	last_word_features = [];
+	last_DOP_features = [];
 
 	for settings in test_settings:
 
 		#import parameters
-		classifier 			= settings[0]
-		features 			= settings[1]
+		classifier  			= settings[0]
+		word_features 			= settings[1]
+		dop_features 			= settings[2]
 
 		#converte the class name and properties into a string, be carefull for punctuation in csv
 		classifier_id = str(classifier)
@@ -35,18 +37,18 @@ def both_features_batch_run(test_settings,method=2):
 		classifier_id = ' '.join(classifier_id.split())
 		
 		#check if a experiment with the current settings was allready conducted
-		if findRun(classifier_id,features,resultsfile=resultsfile):
+		if findRun(classifier_id,resultsfile=resultsfile, word_features=word_features,dop_features=dop_features):
 			print "Experiment with current settings was allready conducted, skipping"
-
 		else:
 
 			#load to csv file to append the results. Do this in the loop to update the file live
 			fd = open(resultsfile,'a')
 
 			#do feature deduction if nesececary
-			if not last_features == features: 
-				X, Xtest, y, r = getBothFeatureData( *getNumBothFeatures(features) , method=method)
-				last_features = features
+			if not (last_word_features == word_features and last_DOP_features  == dop_features):
+				X, Xtest, y, r 	   = getBothFeatureData( word_features,dop_features , method=method)
+				last_word_features = word_features
+				last_DOP_features  = dop_features
 
 			#fit classifier
 			print "Fitting " + classifier_id
@@ -69,8 +71,8 @@ def both_features_batch_run(test_settings,method=2):
 
 			#store results
 			fd.write(settings_to_string(classifier_id,train_accuracy,
-				test_accuracy,fit_time,score_time,features,
-				train_conf_matrix, test_conf_matrix) + "\n")
+				test_accuracy,fit_time,score_time,word_features+dop_features,
+				train_conf_matrix, test_conf_matrix,word_features,dop_features) + "\n")
 
 			#save to csv file and sort csv file
 			fd.close()
@@ -88,9 +90,9 @@ if __name__ == '__main__':
 				  ensemble.RandomForestClassifier(),
 				  #sklearn.ensemble.AdaBoostClassifier(),
 				  sklearn.linear_model.Perceptron(n_iter=60),
-				  svm.SVC(kernel='poly'),
+				  #svm.SVC(kernel='poly'),
 				  svm.SVC(kernel='linear'),
-				  svm.SVC(kernel='sigmoid'),
+				  #svm.SVC(kernel='sigmoid'),
 				  #naive_bayes.GaussianNB(),
 				  #neighbors.nearest_centroid.NearestCentroid(),
 				  svm.SVC(),
@@ -101,11 +103,13 @@ if __name__ == '__main__':
 				  #sklearn.ensemble.AdaBoostClassifier()
 				 	]
 
-	# Maximum number of features: 261396
-	features_set = logRange(261396,15,1)
+	# Maximum number of features: 261396 DOP, 9478 WORDS
+	#features_set = logRange(261395+9478,15,1)
+	features_set = [(w,d) for d in [ 1,      4,     11,     27,     63,    146,    336,    774,
+         1779,   4087,   9389,  21568,  49543, 113799, 261394] for w in [96,  242,  607, 1517, 3793, 9477] ]
 
 	#combine combinatorial (factory because we dont want to duplicate all the classifiers)
-	settings = ( (classifier, features) for features in features_set for classifier  in classifiers )
+	settings = ( (classifier, features[0], features[1]) for features in features_set for classifier  in classifiers )
 
 	both_features_batch_run(settings,method=4)
 	both_features_batch_run(settings,method=2)
