567d70609b5434271f1b463fc950bac215b11719
Ties van Rozendaal <git@tivaro.nl>
2014-12-22 12:01:30 +0100

    Report plotter

--- results/plot_report_graphs.py
+++ results/plot_report_graphs.py
@@ -1,12 +1,18 @@
 from plot_results import *
 
+#settings!
+proportional_features = True  #absolute or proportional features on x axis
+plot_apart			  = True #plot word and dop in same or different plot
 
+#load results
 w_table = get_result_table(filename='M4_baseline_results.csv')
 d_table = get_result_table(filename='M4_classifier_results.csv')
 b_table = get_result_table(filename='M4_combined_features_results.csv')
 
 #Dop features: 261394
 # word featuers:9477
+#Hack
+w_table =w_table[w_table['features']<=9477]
 
 #classifiers to plot
 c_id_list = ["MLPClassifier(batch_size=100; l2decay=0; loss='cross_entropy'; lr=0.1; n_hidden=800; output_layer=None; verbose=0)",
@@ -19,12 +25,32 @@ c_names_list = ['MLP','linear SVM','rbf SVM', 'Perceptron','Decision Tree']
 
 color_list = ['purple','green','turquoise','red','blue','gray']
 
+if proportional_features:
+	s0 = 1.0/9477
+	s1 = 1.0/261394 
+else:
+	s0=s1=1.0
+
+if plot_apart:
+	f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+else:
+	f, ax1 = plt.subplots()
+	ax2 = ax1
+
 for n,c_id in enumerate(c_id_list):
 
 	c_table = w_table[w_table['classifier_id']==c_id]
-	plt.plot(1.0/261394 * c_table['features'],c_table['test_accuracy'],label=c_names_list[n],color=color_list[n],linestyle='_')
+
+	print c_table['features']
+	print c_table['test_accuracy']
+	ax1.plot(s0 * c_table['features'],c_table['test_accuracy'],label=c_names_list[n],color=color_list[n],linestyle='-')
 
 	c_table = d_table[d_table['classifier_id']==c_id]
-	plt.plot(1.0/9477 * c_table['features'],c_table['test_accuracy'],label=c_names_list[n],color='m',linestyle='-')
+	ax2.plot(s1* c_table['features'],c_table['test_accuracy'],label=c_names_list[n],color=color_list[n],linestyle='--')
 
+plt.ylabel('test accuracy')
+if proportional_features:
+	plt.xlabel('proportion of features used')
+else:
+	plt.xlabel('# features used')
 plt.show()
